bf9440e98d13cda988258afdca0d89a731adbe4c
Hubert Deng <hubertdeng123@gmail.com>
2022-09-07 13:45:33 -0500

    test commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sentry_sdk
 app = Flask(__name__)
 
 sentry_sdk.init(
-    dsn="http://2d397caad47e42708fbbb6dbe21e47bd@127.0.0.1:9000/2",
+    dsn="https://fcb09c140c634aa581724271daa9f1b3@o1383316.ingest.sentry.io/6727714",
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
@@ -23,7 +23,15 @@ def error():
 @app.route("/exception")
 def division_by_zero():
     division_by_zero = 1 / 0
-    return
+    return "Should Not Return", 200
+
+@app.route("/sentry-exception")
+def sentry_exception():
+    try:
+        division_by_zero = 1 / 0
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+    return "Sentry Exception", 200
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
