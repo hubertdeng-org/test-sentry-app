45544daa4551ba28f2d7487674118142e8ed08ae
Hubert Deng <hubertdeng123@gmail.com>
2023-08-23 11:05:39 -0700

    let's try this

--- main.py
+++ main.py
@@ -1,46 +1,45 @@
 from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
 import sentry_sdk
 
 app = Flask(__name__)
 
 sentry_sdk.init(
-    dsn="https://fcb09c140c634aa581724271daa9f1b3@o1383316.ingest.sentry.io/6727714",
+    dsn="http://1262ac3315a842d886c7595186ddd33e@127.0.0.1:9000/2",
 
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    environment="production"
+    integrations=[
+        FlaskIntegration(),
+    ],
+    traces_sample_rate=1.0
 )
 
 @app.route("/")
 def index():
-    with sentry_sdk.configure_scope() as scope:
-        scope.transaction.name = "LoadHomePage"
     return "Hello World :)", 200
 
 @app.route("/error")
 def error():
-    with sentry_sdk.configure_scope() as scope:
-        scope.transaction.name = "LoadErrorPage"
     return "Error", 400
 
 @app.route("/exception")
 def division_by_zero():
-    with csentry_sdk.onfigure_scope() as scope:
-        scope.transaction.name = "ExceptionDivisionByZero"
     division_by_zero = 1 / 0
     return "Should Not Display", 200
 
 @app.route("/sentry-exception")
 def sentry_exception():
-    with sentry_sdk.configure_scope() as scope:
-        scope.transaction.name = "SentryExceptionDivisionByZero"
     try:
         division_by_zero = 1 / 0
     except Exception as e:
         sentry_sdk.capture_exception(e)
     return "Sentry Exception", 200
+
+@app.route('/post', methods=['POST'])
+def post():
+    return "Test :1234"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
